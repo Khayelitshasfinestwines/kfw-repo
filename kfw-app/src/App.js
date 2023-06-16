4c53f20e255c12766193b87f4b2e3f36c2733705
Noam-tal <noamptal@gmail.com>
2023-06-16 16:47:10 +0200

    dfg

--- kfw-app/src/App.js
+++ kfw-app/src/App.js
@@ -1,11 +1,19 @@
 
 import './App.css';
+import NavBar from './components/navbar.js'
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div >
+    <NavBar className='text-light'></NavBar>
     </div>
+    
   );
 }
 
