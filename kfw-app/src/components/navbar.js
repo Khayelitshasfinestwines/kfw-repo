39eaf725c2e7e0d492eb747cb578df3f12d0c883
Noam-tal <noamptal@gmail.com>
2023-06-16 21:05:53 +0200

    saf

--- kfw-app/src/components/navbar.js
+++ kfw-app/src/components/navbar.js
@@ -5,14 +5,14 @@ import './navbar.css'
 const NavBar = () =>{
 
 return (
-    <div className='text-light absolute-nav-bar'>
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark p-4 absolute-nav-bar">
-  <a class="navbar-brand text-light" href="#">Navbar</a>
+    <div className='text-light sticky-nav-bar '>
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3 position-sticky">
+  <a class="navbar-brand text-light" href="#">Logo</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse nav-bar-text-elements" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto text-light">
       <li class="nav-item active">
         <a class="nav-link text-light" href="#">Home <span class="sr-only">(current)</span></a>
