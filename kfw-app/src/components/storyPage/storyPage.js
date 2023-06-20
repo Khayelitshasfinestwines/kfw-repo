5552e6a990fef4e8267b8a96c7d9a2ecc523a07f
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-06-20 14:19:31 +0200

    pushing

--- kfw-app/src/components/storyPage/storyPage.js
+++ kfw-app/src/components/storyPage/storyPage.js
@@ -1,18 +1,16 @@
 import React from 'react'
+import './storyPage.css';
 
 const StoryPage = ()=>{
 
     return (
         <div>
-            Our Story
+            <div className="container">
+            <img src="storyimage1.png" alt="story header" style={{ width: '100%' }} />
+                <div className="centered">Centered</div>
+            </div>
         </div>
     )
 }
-
-
-
-
-
-
 
 export default StoryPage
