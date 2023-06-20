cc8f3b3862951767871d2f72034e7acde0fa31b0
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-06-20 23:00:33 +0200

    idk if i commited earlier

--- kfw-app/src/components/storyPage/storyPage.js
+++ kfw-app/src/components/storyPage/storyPage.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './storyPage.css';
+import StoryPageImage from './storyPageImages/storypage2.jpg';
 
 const StoryPage = ()=>{
 
     return (
         <div>
             <div className="container">
-            <img src="storypageimage1.png" alt="story header" style={{ width: '100%' }} />
-                <div className="centered">Centered</div>
+            <img src={StoryPageImage} alt="story header" style={{ width: '100%' }} />
+                <div className="centered">Our Story</div>
             </div>
         </div>
     )
