060047e1cfb0ebf1ab8a2d96dae76f29a507a207
Noam-tal <noamptal@gmail.com>
2023-06-20 09:51:28 +0200

    fixed navar toggler

--- kfw-app/src/components/Navbar/navbar.js
+++ kfw-app/src/components/Navbar/navbar.js
@@ -1,48 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './navbar.css';
 
-import React from 'react'
-import './navbar.css'
-import {Link} from "react-router-dom"
+const NavBar = () => {
+  return (
+    <div className='text-light sticky-nav-bar'>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3 position-sticky">
+        <Link className="navbar-brand text-light" to="/">Logo</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-
-const NavBar = () =>{
-
-return (
-    <div className='text-light sticky-nav-bar '>
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark p-3 position-sticky">
-  <Link class="navbar-brand text-light" to="/">Logo</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse nav-bar-text-elements" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto text-light">
-      <li class="nav-item active nav-bar-spacing">
-        <Link class="nav-link text-light" to="/shopping"> Shop</Link>
-      </li>
-      <li class="nav-item active nav-bar-spacing">
-        <Link class="nav-link text-light" to="/tastings">Wine Tastings</Link>
-      </li>
-      <li class="nav-item active nav-bar-spacing">
-        <Link class="nav-link text-light" to="/story"> Our Story</Link>
-      </li>
-      <li class="nav-item nav-bar-spacing">
-        <Link class="nav-link text-light" to="/newsletter">Newsletter</Link>
-      </li>
-      <li class="nav-item nav-bar-spacing">
-        <Link class="nav-link text-light" to="/international">International</Link>
-      </li>
-      <li class="nav-item nav-bar-spacing">
-        <Link class="nav-link text-light" to="/contact">Contact Us</Link>
-      </li>
-      <li class="nav-item nav-bar-spacing">
-        <Link class="nav-link text-light" to="/login">Login</Link>
-      </li>
-    </ul>
-  </div>
-</nav>
+        <div className="collapse navbar-collapse nav-bar-text-elements" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto text-light">
+            <li className="nav-item active nav-bar-spacing">
+              <Link className="nav-link text-light" to="/shopping">Shop</Link>
+            </li>
+            <li className="nav-item active nav-bar-spacing">
+              <Link className="nav-link text-light" to="/tastings">Wine Tastings</Link>
+            </li>
+            <li className="nav-item active nav-bar-spacing">
+              <Link className="nav-link text-light" to="/story">Our Story</Link>
+            </li>
+            <li className="nav-item nav-bar-spacing">
+              <Link className="nav-link text-light" to="/newsletter">Newsletter</Link>
+            </li>
+            <li className="nav-item nav-bar-spacing">
+              <Link className="nav-link text-light" to="/international">International</Link>
+            </li>
+            <li className="nav-item nav-bar-spacing">
+              <Link className="nav-link text-light" to="/contact">Contact Us</Link>
+            </li>
+            <li className="nav-item nav-bar-spacing">
+              <Link className="nav-link text-light" to="/login">Login</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
-    
-);
-
+  );
 };
+
 export default NavBar;
