b22002ae7849996adc06b625b2d10cd5f2916e3a
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-06-24 01:07:13 +0200

    did some styling for story page

--- kfw-app/src/components/storyPage/storyPage.js
+++ kfw-app/src/components/storyPage/storyPage.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React from 'react';
 import './storyPage.css';
 import StoryPageImage from './storyPageImages/storypage2.jpg';
+import QRCode from './storyPageImages/StoryQR.jpg';
 
-const StoryPage = ()=>{
+const StoryPage = () => {
+  return (
+    <div>
+      <div className="heading-container">
+        <img src={StoryPageImage} alt="story header" style={{ width: '100%' }} />
+        <div className="centered">Our Story</div>
+      </div>
 
-    return (
-        <div>
-            <div className="container">
-            <img src={StoryPageImage} alt="story header" style={{ width: '100%' }} />
-                <div className="centered">Our Story</div>
-            </div>
+      <div className="container">
+        <p class="h1 mt-5 header">The Origin</p>
+
+        <div className="text">
+          <p>Dummy</p>
         </div>
-    )
-}
 
-export default StoryPage
+        <img src={QRCode} alt="QR Code" style={{ width: '50%' }} />
+      </div>
+    </div>
+  );
+};
+
+export default StoryPage;
