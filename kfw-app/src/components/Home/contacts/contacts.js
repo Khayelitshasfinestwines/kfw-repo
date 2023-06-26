dca1e79dbd860269f1743010d5b3da8b6312a5e4
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-06-26 21:23:54 +0200

    fully functional and styled social media icons finished

--- kfw-app/src/components/Home/contacts/contacts.js
+++ kfw-app/src/components/Home/contacts/contacts.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './contacts.css';
+import { BsFacebook } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
 
 const Contacts = () => {
   return (
@@ -7,14 +9,22 @@ const Contacts = () => {
       <div className="leftcontainer">
         <div className="leftcontent">
           <p className="header">Contact Us</p>
-          <p className="email">INSERT EMAIL</p>
-          <p className="phone">INSERT NUMBER</p>
+          <p className="email">lindilendzaba@gmail.com</p>
+          <p className="phone">+27 73 081 3691</p>
         </div>
       </div>
 
       <div className="rightcontainer">
         <div className="rightcontent">
           <p className="header">Socials</p>
+          <div className="social-icons">
+            <a href="https://www.instagram.com/khayelitshasfinestwines" className="btn btn-primary btn-lg instagram-icon">
+              <BsInstagram></BsInstagram>
+            </a>
+            <a href="https://www.facebook.com/KhayelitshasFinestWines/" className="btn btn-primary btn-lg facebook-icon">
+              <BsFacebook></BsFacebook>
+            </a>
+          </div>
         </div>
       </div>
     </div>
