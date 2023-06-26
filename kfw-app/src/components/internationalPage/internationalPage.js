051b1faa5d17c0334d52772ea4c617c5e56f4223
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-06-26 23:32:28 +0200

    added begginning of international page

--- kfw-app/src/components/internationalPage/internationalPage.js
+++ kfw-app/src/components/internationalPage/internationalPage.js
@@ -1,18 +1,26 @@
 import React from 'react'
+import './internationalPage.css';
+import InternationalImage from './internationalPageImages/international4.jpg';
 
 const InternationalPage = ()=>{
-
     return (
         <div>
-            International
+            <div className="heading-container">
+                <img src={InternationalImage} alt="story header" style={{ width: '100%' }} />
+                <div className="centered">International</div>
+            </div>
+
+            <div className="container">
+
+                <div className="text">
+                    <div className = "spacing">
+                        <p>For those interested in our product that live overseas, please contact our partner ______ for information on how we can bring our wine to you.</p>
+                        <p>We offer shipping to ________.</p>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-
-
-
-
-
-
-export default InternationalPage
+export default InternationalPage;
