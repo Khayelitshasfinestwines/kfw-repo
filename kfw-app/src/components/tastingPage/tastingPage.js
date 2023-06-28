9c57f7044cec8f98edbc023e01212b32b77db09d
Noam-tal <noamptal@gmail.com>
2023-06-28 17:22:43 +0200

    tastingPage stuff

--- kfw-app/src/components/tastingPage/tastingPage.js
+++ kfw-app/src/components/tastingPage/tastingPage.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import './tastingPage.css'
+import TastingPhoto1 from './newtasting.jpeg'
 
 const TastingsPage = ()=>{
 
     return (
+<div>
         <div>
-            Tastings
+                <div className="heading-container ">
+                    <div className=' container  mt-3 '>
+                    <img className='mt-3 tastings-photo'
+                    src={TastingPhoto1}
+                    alt="Wine Tastings header"
+                />
+                    </div>
+
+                <div className="centered">Wine Tastings</div>
         </div>
+
+        <div className='text-center mt-3 tasting-blurb'>
+            Wine Tastings (explanation)
+        </div>
+
+        <div className='button-center'>
+
+                <button className='btn btn-dark mt-3 mb-3'>Book Now</button>
+
+
+        </div>
+
+        </div>
+     
+    </div>
     )
 }
 
