efdb015b17c9b08b614d2260b5324e3b60cbf4d4
elsie-eghan <129563817+elsie-eghan@users.noreply.github.com>
2023-07-01 20:10:27 +0200

    possible fix

--- kfw-app/src/components/shoppingPage/redWine /redWine.js
+++ kfw-app/src/components/shoppingPage/redWine /redWine.js
@@ -5,14 +5,19 @@ import { Link } from 'react-router-dom';
 
 const RedWine = () => {
   return ( 
-  <> <h3><div className='center2'> Red Wine </div></h3>  
+  <>  
   <div className="container2">
       <div className="column1">
         <div>
           <img src={StoryImage} alt="Story Image" style={{ width: '500px' }} />
         </div>
       </div>
-      <div className="column2">Column 2</div>
+      
+      <div className="column2">
+      <h3><div > R e d  B l en d </div></h3> 
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+
+The standard chunk o</div>
     </div></>
   );
 };
