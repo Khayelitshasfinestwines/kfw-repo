b3b87282bf8c6346d42166d889d0621f0c9829a2
dhollaractualize <106189949+dhollaractualize@users.noreply.github.com>
2023-07-02 23:12:29 +0200

    completed contact page

--- kfw-app/src/components/contactPage/contactPage.js
+++ kfw-app/src/components/contactPage/contactPage.js
@@ -3,6 +3,11 @@ import './contactPage.css';
 import ContactPageImage from './contactPageImages/contactpageimage1.jpg';
 import { BsFillEnvelopeFill } from 'react-icons/bs';
 import { BsFillTelephoneFill } from 'react-icons/bs';
+import { BsFacebook } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import { BsTiktok } from 'react-icons/bs';
+import { BsTwitter } from 'react-icons/bs';
+
 
 const ContactPage = () => {
   return (
@@ -15,9 +20,9 @@ const ContactPage = () => {
       <div className="container">
         <div className="text">
           <div className="spacing">
-            <p>If you have any questions about our products or are interested in booking a wine tour, please use the contact below</p>
+            <p>If you have any questions about our products or are interested in booking a wine tour, please use the contact below:</p>
             </div>
-            <div className="contact-container mt-2">
+            <div className="contact-container">
               <div className="contact-row name">
                 <p>Lindile Ndzaba</p>
               </div>
@@ -32,6 +37,23 @@ const ContactPage = () => {
           </div>
           <p className="caption mt-5">Connect With Us</p>
           <p className="caption-sub">Explore our Wine Journey on Social Media</p>
+
+          <div className="contact-page-icons mb-5">
+            <a href="https://www.facebook.com/KhayelitshasFinestWines/" className="facebook-icon">
+              <BsFacebook></BsFacebook>
+            </a>
+            <a href="https://www.instagram.com/khayelitshasfinestwines" className="instagram-icon">
+              <BsInstagram></BsInstagram>
+            </a>
+            <a href="https://www.tiktok.com/@khayelitshasfinestwines" className="tiktok-icon">
+              <BsTiktok></BsTiktok>
+            </a>
+            <a href="https://twitter.com/khayelitshawine" className="twitter-icon">
+              <BsTwitter></BsTwitter>
+            </a>
+          </div>
+
+
         </div>
       </div>
     </div>
